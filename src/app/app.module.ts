26d2389d01f3e7bf249cfc34e0fe44bb2b2fc672
VERNAY <alexis.vernay@cgi.com>
2019-05-20 15:25:35 +0200

    Création projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+import { SharedModule } from './shared/shared.module';
+import { ClientsModule } from './clients/clients.module';
+import { PrestationsModule } from './prestations/prestations.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiModule,
+    PageNotFoundModule,
+    SharedModule,
+    ClientsModule,
+    PrestationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
